0dd7061e26ccc2020ea9fd49fd9292933fd3e4ca
Abdul Haqani <ahaqa011@uottawa.ca>
2020-08-12 02:48:18 -0400

    just setting up stuff

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../scss/sidebar.scss';
+export const Sidebar = () => {
+  return (
+    <div>
+      <div className="side-bar-container">
+        <a href="#">My tag</a>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
