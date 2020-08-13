1e1e23e0b06caccd9e090edc35de26f1d1815b2a
Abdul Haqani <ahaqa011@uottawa.ca>
2020-08-13 01:00:08 -0400

    minor changes

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import '../../scss/sidebar.scss';
+
 export const Sidebar = () => {
   return (
     <div>
       <div className="side-bar-container">
         <div className="link-row">
           <ul className="links">
-            <li className="link">1</li>
-            <li className="link">2</li>
-            <li className="link">3</li>
-            <li className="link">4</li>
+            <li className="link">
+              <i className="small material-icons">home</i>
+            </li>
+            <li className="link">
+              <i className="small material-icons">home</i>
+            </li>
+            <li className="link">
+              <i className="small material-icons">home</i>
+            </li>
+            <li className="link">
+              <i className="small material-icons">home</i>
+            </li>
           </ul>
         </div>
         <ul>
           <li className="sidebar-navigation">
-            <a href="#">
+            <a href="">
               <h4>Link 1</h4>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="">
               <h4>Link 2</h4>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="">
               <h4>Link 3</h4>
             </a>
           </li>
           <li>
-            <a href="#">
+            <a href="">
               <h4>Link 4</h4>
             </a>
           </li>
