02b59b805afdc1420fb64994e704ae947f3a0ff1
Abdul Haqani <ahaqa011@uottawa.ca>
2020-08-17 15:39:49 -0400

    home-container

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,18 @@ import '../scss/home.scss';
 export const Home = () => {
   return (
     <div className="main-content">
-      <div className="home-container">
-        <h2 className="home-title">Abdul Haqani</h2>
-      </div>
-      <div>
-        <p className="paragraph-text">
-          Recent graduate from the university of Ottawa, designer to my core,
-          permanent student of the craft. I love to learn, create, design and
-          overcome challenges while developing new things. Feel free to contact
-          me!
-        </p>
+      <div className="home-container z-depth-2">
+        <div className="home-content">
+          <h2 className="home-title">Abdul Haqani</h2>
+        </div>
+        <div>
+          <p className="paragraph-text">
+            Recent graduate from the university of Ottawa, designer to my core,
+            permanent student of the craft. I love to learn, create, design and
+            overcome challenges while developing new things. Feel free to
+            contact me!
+          </p>
+        </div>
       </div>
     </div>
   );
