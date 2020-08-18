5d72d8423ed817e32e41db42a2e3533b2ce6e94e
Abdul Haqani <ahaqa011@uottawa.ca>
2020-08-18 02:09:27 -0400

    big push, contact done, home done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,13 +4,51 @@ import '../scss/default.scss';
 
 export const Contact = () => {
   return (
-    <div className="main-content center-align">
-      <p className="paragraph-text">
-        Recent graduate from the university of Ottawa, designer to my core,
-        permanent student of the craft. I love to learn, create, design and
-        overcome challenges while developing new things. Feel free to contact
-        me!
-      </p>
+    <div className="contact-background">
+      <div>
+        <h1 className="center-align contact-head">CONTACT</h1>
+        <div>
+          <p className="contact-paragraph center-align">
+            Speaking with other developers, designers, entrepreneurs and people
+            in general is always refreshing. Contact me through this form if you
+            have any recomendations, critiques or if you just want to say hello.
+            If you are interested in my work, it is all posted on Github and if
+            you wish to connect with me, feel free to send me an invitation
+            through LinkedIn. If you are interested in working together or in
+            the services I offer please send me a message as I would love to get
+            in touch with you.
+          </p>
+        </div>
+        <div className="form contact-form center-align">
+          <div className="input-field input-edit">
+            <input
+              placeholder="Name"
+              id="name"
+              type="text"
+              className="validate"
+            />
+            <label for="first_name">Name</label>
+          </div>
+
+          <div className="input-field input-edit">
+            <input id="email" type="email" className="validate" />
+            <label for="email">Email</label>
+          </div>
+
+          <div class="input-field input-edit ">
+            <textarea id="Message" class="materialize-textarea"></textarea>
+            <label for="Message">Message</label>
+          </div>
+          <button
+            class="btn waves-effect waves-light grey darken-1 submit-btn-edit"
+            type="submit"
+            name="action"
+          >
+            Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,22 +9,22 @@ export const Home = (props) => {
     <div className={background}>
       <div className="home-container">
         <div className="home-intro-container z-depth-2">
-          <div className="home-content">
-            <div className="home-title">Abdul Haqani</div>
-            <p className="intro-text">
-              University of Ottawa Alum, Junior Software Engineer
-            </p>
+          <div className="home-title">Abdul Haqani</div>
+          <p className="intro-text">
+            University of Ottawa Alum, Junior Software Engineer
+          </p>
 
-            <p className="paragraph-text">
-              Recent graduate from the university of Ottawa, designer to my
-              core, permanent student of the craft. I love to learn, create,
-              design and overcome challenges while developing new things. Feel
-              free to contact me!
-            </p>
-          </div>
+          <p className="paragraph-text">
+            Recent graduate from the university of Ottawa, designer to my core,
+            permanent student of the craft. I love to learn, create, design and
+            overcome challenges while developing new things. Feel free to
+            contact me!
+          </p>
         </div>
       </div>
-      <Contact />
+      <div className="contact">
+        <Contact />
+      </div>
     </div>
   );
 };
