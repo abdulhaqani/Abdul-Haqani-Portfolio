15db3685a9241b6804670d8b3a6ac7f47e67e55d
Abdul Haqani <ahaqa011@uottawa.ca>
2020-08-20 13:50:03 -0400

    layout + about

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,8 +3,18 @@ import '../scss/aboutMe.scss';
 
 export const AboutMe = () => {
   return (
-    <div className="main-content center-align">
-      <p> AboutMe </p>
+    <div className="about-me-background center-align">
+      <div>
+        <div className="about-me-header condensed thin">About Abdul Haqani</div>
+        <div>Navigation images</div>
+        <div className="about-me-subheader">Developer to the core.</div>
+      </div>
+      <p className="about-paragraph">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam animi
+        voluptate nam non porro itaque, officia molestiae earum laudantium
+        iusto! Dolor id illo unde aliquid. Voluptatum ab accusamus cupiditate
+        asperiores?
+      </p>
     </div>
   );
 };
