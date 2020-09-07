d951143dda10095ab68a4dbebf77211b66c81e39
Abdul Haqani <ahaqa011@uottawa.ca>
2020-09-07 02:29:13 -0400

    possible fix

--- server.js
+++ server.js
@@ -13,17 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
-  // set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-}
-
 // simple backend to send the emails
-app.get('/');
 app.post('/contact', (req, res) => {
   let transporter = nodeMailer.createTransport({
     host: 'smtp.gmail.com',
@@ -66,5 +56,14 @@ app.post('/contact', (req, res) => {
 
   res.end();
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(process.env.PORT || 5000);
