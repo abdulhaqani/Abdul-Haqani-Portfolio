de816129348c09dc632be9aadc2e6d895d964941
abdulhaqani <ahaqa011@uottawa.ca>
2023-03-18 03:26:55 -0400

    styling

--- client/src/components/AboutContent/Experience.js
+++ client/src/components/AboutContent/Experience.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css/dist/js/materialize.min.js";
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const Experience = () => {
   useEffect(() => {
-    var myModal = document.querySelectorAll(".modal");
+    var myModal = document.querySelectorAll('.modal');
     const moodalInstance = M.Modal.init(myModal, { preventScrolling: true });
   }, []);
   //<h1 className="freelance-card italics">Freelancing</h1>;
@@ -22,7 +22,7 @@ const Experience = () => {
               <div className="italics">04/20/2022 - Current</div>
 
               <img
-                src={require("../../images/qlik-logo.png")}
+                src={require('../../images/qlik-logo.png')}
                 className="qlik-logo"
                 alt=""
               />
@@ -38,7 +38,7 @@ const Experience = () => {
         <div id="modal-qlik" className="modal modal-qlik modal-orientation">
           <div className="modal-content my-modal grey-text text-lighten-2">
             <img
-              src={require("../../images/qlik-logo.png")}
+              src={require('../../images/qlik-logo.png')}
               className="qlik-logo"
               alt=""
             />
@@ -80,18 +80,20 @@ const Experience = () => {
         </div>
         <div className="col s2"></div>
 
-        <div className="col s4 card card-hover modal-cibc auto-center">
+        <div className="col s4 card card-hover modal-cibc auto-center experience-card">
           <a className="modal-trigger" href="#modal-cibc">
-            <div className="card-content white-text center-align waves-effect waves-light">
-              <span className="card-title">CIBC Capital Markets </span>
+            <div className="card-content white-text center-align waves-effect waves-light experience-card">
+              <span className="card-title" style={{ 'font-size': '20px' }}>
+                CIBC Capital Markets{' '}
+              </span>
               <div className="italics">01/10/2020 - 04/20/2022</div>
 
               <img
-                src={require("../../images/cibc.png")}
+                src={require('../../images/cibc.png')}
                 className="cibc-logo"
                 alt=""
               />
-              <div>
+              <div className="arrow-icon">
                 <i className="material-icons right-align right-arrow">
                   keyboard_arrow_right
                 </i>
@@ -104,7 +106,7 @@ const Experience = () => {
         <div id="modal-cibc" className="modal modal-cibc modal-orientation">
           <div className="modal-content my-modal grey-text text-lighten-2">
             <img
-              src={require("../../images/cibc.png")}
+              src={require('../../images/cibc.png')}
               className="statistics-canada-logo"
               alt=""
             />
@@ -155,11 +157,11 @@ const Experience = () => {
               <div className="italics">01/01/2019 - 04/30/2020</div>
 
               <img
-                src={require("../../images/canada.png")}
+                src={require('../../images/canada.png')}
                 className="canada-logo"
                 alt=""
               />
-              <div>
+              <div className="arrow-icon">
                 <i className="material-icons right-align right-arrow">
                   keyboard_arrow_right
                 </i>
@@ -175,7 +177,7 @@ const Experience = () => {
         >
           <div className="modal-content my-modal grey-text text-lighten-2">
             <img
-              src={require("../../images/statistics-canada.png")}
+              src={require('../../images/statistics-canada.png')}
               className="statistics-canada-logo"
               alt=""
             />
@@ -233,74 +235,6 @@ const Experience = () => {
           </div>
         </div>
         <div className="col s2"></div>
-
-        <div className="col s4 card card-hover freelancing-bg center-align auto-center">
-          <a
-            className="modal-trigger  grey-text text-lighten-2"
-            href="#modal-freelancing"
-          >
-            <div className="card-content center-align waves-effect waves-purple card-fit">
-              <span className="card-title">Freelancing</span>
-              <div className="italics">01/05/2020 - Current</div>
-
-              <div>
-                <i className="material-icons right-align right-arrow">
-                  keyboard_arrow_right
-                </i>
-                <br />
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div
-          id="modal-freelancing"
-          className="modal modal-orientation freelancing-bg grey-text text-lighten-1"
-        >
-          <div className="modal-content my-modal">
-            <h2 className="center-align my-modal-header underline italics">
-              Freelancing
-            </h2>
-            <h4>Mercury Web Scraping</h4>
-            <p className="modal-paragraph">
-              <br />
-              Mercury Scraping is a full stack app with a Python Django
-              PostgreSQL stack that I co-developed with a colleage. Public
-              sentiment data is something which in modern times is readily
-              available on the many social medias that are frequented on a
-              regular basis. These sentiments are valuable to certain people,
-              especially to those people in the field of journalism, who
-              regularly use these sentiments to determine where to head next. It
-              is for this reason that this application was developed, it aims to
-              dramatically reduce the time that journalists normally have to
-              spend manually looking for public sentiment data.
-            </p>
-            <p className="modal-paragraph">
-              The app uses web scrapers to pull data from various social medias
-              and perform analytics on it. The web scrapers are able to filter
-              out based on the content of the posts, the date of posting,
-              whether the post was positive or negative, among other things. The
-              social medias that were included in the web scraping were Twitter
-              and Reddit. The data and web scrapers were interfaced with the
-              application to display information to the client.
-            </p>
-            <div className="chip-align">
-              <div className="chip">Python</div>
-              <div className="chip">Django </div>
-              <div className="chip">PostgreSQL</div>
-              <div className="chip">Web-Scraping</div>
-              <div className="chip">API</div>
-            </div>
-          </div>
-          <div className="modal-footer freelancing-bg">
-            <a
-              href="#!"
-              className="modal-close waves-effect waves-purple btn-flat grey-text text-lighten-1"
-            >
-              Agree
-            </a>
-          </div>
-        </div>
       </div>
     </div>
   );
